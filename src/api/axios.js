d6bbecbd341a09749eb4b98e083e93ff9ba25969
Jimmy Quadros <jimmyquadros@hotmail.com>
2023-10-04 13:24:59 -0400

    update for cyclic use

--- src/api/axios.js
+++ src/api/axios.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const BASE_URL = 'http://localhost:5000';
+const BASE_URL = 'https://famous-crab-frock.cyclic.cloud'; //'http://localhost:5000';
 
 export default axios.create({
     baseURL: BASE_URL,
